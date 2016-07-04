3c19cf59f863a0c0dd824b145a3425099cdac39d
agubertoni <agubertoni@gmail.com>
2016-07-04 10:45:15 -0300

    No arrancaba morris

--- client/templates/flow_chart.js
+++ client/templates/flow_chart.js
@@ -7,38 +7,27 @@ Template.flowChart.rendered = function () {
         $('#firstchart').empty();
         var pointer = WaterSensors.find({},{fields:{node:1, reads:1, _id:0},sort:{node:1}}).fetch();
 
-        var uNode = pointer[0].node;
-        //var uFlow = pointer[0].reads[0].flow;
-        var uFlow = WaterSensors.findOne({node:"01"}).reads[0].flow;
-        var data = [];
-        var obj = {};
-
-        obj['node'] = uNode;
-        obj['flow'] = uFlow;
-        data.push(obj);
-
-        /*
         var data = [
             { year: '2008', value: 10},
             { year: '2009', value: 20}
         ];
-        */
+
 
         if (!update) {
         } else {
         }
 
         if (data) {
-            new Morris.Line({
+            new Morris.Bar({
                 // ID of the element in which to draw the chart.
                 element: 'firstchart',
                 // Chart data records -- each entry in this array corresponds to a point on
                 // the chart.
                 data:    data,
                 // The name of the data record attribute that contains x-values.
-                xkey:    'node',
+                xkey:    'year',
                 // A list of names of data record attributes that contain y-values.
-                ykeys:   ['flow'],
+                ykeys:   ['value'],
                 // Labels for the ykeys -- will be displayed when you hover over the
                 // chart.
                 labels:  ['Value'],
